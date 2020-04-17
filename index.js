3ca14ae0059f5b174ed3ea29869414bc491b74f7
Ben Moore <btmoore40@gmail.com>
2020-04-16 21:51:30 -0400

    Adding Mapping HTML Form and added to setMapping POST

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 const port = process.env.PORT;
