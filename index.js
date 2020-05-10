11187a302adc5b9ea2b2d7a58c183dae39317076
Ben Moore <btmoore40@gmail.com>
2020-05-10 17:03:33 -0400

    Fixed html mapping form, added addNewRow to Post

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// Config variables set via heroku
+const HEROKU_AIR_KEY = process.env.AIRTABLE_KEY;
+const HEROKU_AIR_BASE_ID = process.env.AIRTABLE_BASE;
+const HEROKU_TABLE_NAME = process.env.AIRTABLE_TABLE_NAME;
+const MANYC_NEW_REQ_URL = process.env.MANYC_NEW_REQ;
+const MANYC_UPDATE_REQ_URL = process.env.MANYC_UPDATE_REQ;
+const MANYC_DELETE_REQ_URL = process.env.MANYC_DELETE_REQ;
+
 const path = require('path');
 const express = require('express');
+const axios = require('axios');
+const Airtable = require('airtable');
+const AirtableHelper = require('./src/addAirTableRow')
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -20,37 +31,15 @@ app.get('/', (req, res, next) => {
 let clientColumnMapping = {};
 
 app.get('/api/getColumnMapping', (req, res, next) => {
-  res.send(JSON.stringify(clientColumnMapping));
+  if (Object.keys(clientColumnMapping).length > 0) {
+    res.send(JSON.stringify(clientColumnMapping));
+  } else {
+    res.send('Column Mapping not set yet ...')
+  }
 });
 
-app.post('/api/setColumnMapping', function(req, res) {
+app.post('/api/setColumnMapping', function (req, res) {
   clientColumnMapping = {
-    supportType: req.body.supportType,
-    otherSupport: req.body.otherSupport,
-    community: req.body.community,
-    language: req.body.language,
-    languageOther: req.body.languageOther,
-    region: req.body.region,
-    neighborhood: req.body.neighborhood,
-    zip: req.body.zip,
-    phone: req.body.phone,
-    email: req.body.email,
-    fullName: req.body.fullName,
-    urgency: req.body.urgency,
-    contactMethod: req.body.contactMethod,
-    crossStreet: req.body.crossStreet,
-    timestampCreated: req.body.timestampCreated,
-    source: req.body.source,
-    sourceID: req.body.sourceID,
-    notes: req.body.notes,
-  };
-  console.log(clientMapping);
-  res.send('MANYC Airtable-Gateway Client Mapping Set');
-});
-
-// MANYC Sends to Group Object
-app.post('/api/createRequest', function(req, res) {
-  newRequest = {
     status: req.body.status,
     id: req.body.id,
     supportType: req.body.supportType,
@@ -69,35 +58,51 @@ app.post('/api/createRequest', function(req, res) {
     source: req.body.source,
     sourceID: req.body.sourceID,
   };
+  console.log(clientMapping);
+  res.send('MANYC Airtable-Gateway Client Mapping Set');
+});
+
+// MANYC Sends to Group Object
+app.post('/api/createRequest', function (req, res) {
+  newRequest = {
+    manyc: {
+      status: req.body.status,
+      id: req.body.id,
+      supportType: req.body.supportType,
+      otherSupport: req.body.otherSupport,
+      community: req.body.community,
+      language: req.body.language,
+      languageOther: req.body.languageOther,
+      phone: req.body.phone,
+      email: req.body.email,
+      fullName: req.body.fullName,
+      urgency: req.body.urgency,
+      contactMethod: req.body.contactMethod,
+      crossStreet: req.body.crossStreet,
+      timestampCreated: req.body.timestampCreated,
+      timestampSent: req.body.timestampSent,
+      source: req.body.source,
+      sourceID: req.body.sourceID,
+    }
+  };
   console.log(newRequest);
-  // TODO Create new request with Airtable API
+  AirtableHelper(HEROKU_AIR_KEY, HEROKU_AIR_BASE_ID, HEROKU_TABLE_NAME, [newRequest]);
   res.send('MANYC Airtable-Gateway Needs Request Created');
 });
 
 /* ----------------------------------------------------------------*/
 /* Airtable Polling Section */
 /* ----------------------------------------------------------------*/
-// Config variables set via heroku
-const HEROKU_AIR_KEY = process.env.AIRTABLE_KEY;
-const HEROKU_AIR_BASE_ID = process.env.AIRTABLE_BASE;
-const HEROKU_TABLE_NAME = process.env.AIRTABLE_TABLE_NAME;
-
-const MANYC_NEW_REQ_URL = process.env.MANYC_NEW_REQ;
-const MANYC_UPDATE_REQ_URL = process.env.MANYC_UPDATE_REQ;
-const MANYC_DELETE_REQ_URL = process.env.MANYC_DELETE_REQ;
-
-const axios = require('axios');
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: HEROKU_AIR_KEY}).base(HEROKU_AIR_BASE_ID);
-
+const base = new Airtable({ apiKey: HEROKU_AIR_KEY }).base(HEROKU_AIR_BASE_ID);
 const ChangeDetector = require('airtable-change-detector');
+
 const airtableGatewayDetector = new ChangeDetector(
   base(HEROKU_TABLE_NAME), {
-    writeDelayMs: 100,
-    metaFieldName: 'Meta', // Defaults to `Meta`
-    lastModifiedFieldName: 'Last Modified', // Defaults to `Last Modified`
-    lastProcessedFieldName: 'Last Processed',
-  },
+  writeDelayMs: 100,
+  metaFieldName: 'Meta', // Defaults to `Meta`
+  lastModifiedFieldName: 'Last Modified', // Defaults to `Last Modified`
+  lastProcessedFieldName: 'Last Processed',
+},
 );
 
 airtableGatewayDetector.pollWithInterval(
@@ -115,6 +120,6 @@ airtableGatewayDetector.pollWithInterval(
     });
 
     // If doing many Airtable writes, be careful of 5rps rate limit
-    return Promise.all(promises).catch;
+    return Promise.all(promises);
   },
 );
