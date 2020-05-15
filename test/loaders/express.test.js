516a9642e68b80a172b04ff2baafee9b405fa467
Aaron Young <brainomite@gmail.com>
2020-05-15 08:45:29 -0400

    test: Add tests for the / route

---
+++ test/loaders/express.test.js
@@ -0,0 +1,19 @@
+'use strict'
+const request = require('supertest');
+const { expect } = require('chai');
+const expressLoader = require('../../src/loaders/express');
+const app = require('express')()
+
+expressLoader(app)
+
+describe('GET /', async () => {
+  it('return result 200 status', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.eql({});
+    expect(res.type).to.equal('text/html')
+    expect(res.text).to.include('<!doctype html>')
+    expect(res.text).to.include('MANYC Airtable-Gateway Mapping Form')
+
+  });
+});
