7e114d8ac57d19f92761c7049793c3e1225232d5
Aaron Young <brainomite@gmail.com>
2020-05-16 21:43:00 -0400

    test: Cretate test for /api/getColumnMapping

---
+++ test/routes/api/getColumnMapping.test.js
@@ -0,0 +1,18 @@
+'use strict';
+const request = require('supertest');
+const { expect } = require('chai');
+const expressLoader = require('../../../src/loaders/express');
+const app = require('express')();
+
+expressLoader(app);
+
+describe('GET /api/getColumnMapping', async () => {
+  it('return result 200 status', async () => {
+    const res = await request(app).get('/api/getColumnMapping');
+
+    expect(res.status).to.equal(200);
+    expect(res.body).to.eql({});
+    expect(res.type).to.equal('text/html');
+    expect(res.text).to.include('Column Mapping not set yet ...');
+  });
+});
