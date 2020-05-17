1f80b8872bacb48b48a987c2db6134d18de6807a
Aaron Young <brainomite@gmail.com>
2020-05-17 09:10:29 -0400

    style: Fix linting issues

--- test/service/airtablePoller.examples.js
+++ test/service/airtablePoller.examples.js
@@ -1,131 +1,132 @@
-'use strict'
-
+'use strict';
+/* eslint-disable max-len */
 module.exports = {
   newRecord: [
     {
       fields: {
         'Unique ID': 985,
-        Cell: '(516) 555-1234',
+        'Cell': '(516) 555-1234',
         'Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.': [
-          'recXXXXXXXXXXXXXX'
+          'recXXXXXXXXXXXXXX',
         ],
         'What type(s) of support are you seeking?': [
           'Deliver groceries or supplies to me',
-          'Pick up a prescription for me'
+          'Pick up a prescription for me',
         ],
         'Cross Streets': '120th & 7th',
         'What borough/region are you in?': 'Bronx',
         'What neighborhood?': [
-          'recxxxxxxxxxxxxx2'
+          'recxxxxxxxxxxxxx2',
         ],
         'How soon do you need support?': 'In the next few days (3 - 5 Days)',
         'Anything else you want to share about your situation at this time?': 'Pie is great',
         'Timestamp Created': '2020-05-12T00:07:49.000Z',
         'Automation Logs': 'Dispatched: 11-05-2020 08:31 PM',
-        Name: 'John',
+        'Name': 'John',
         'Full Name': 'John Doe ',
         'Dispatched Time': '2020-05-12T00:25:32.000Z',
         'Automation Updated Timestamp': '2020-05-17T11:53:51.000Z',
-        'AirTable Gateway Last Modified': '2020-05-17'
-      }
-    }
+        'AirTable Gateway Last Modified': '2020-05-17',
+      },
+    },
   ],
   twoRecords: [
     {
       id: 'recxxxxxxxxxxx',
       fields: {
         'Unique ID': 985,
-        Cell: '(516) 555-1234',
+        'Cell': '(516) 555-1234',
         'Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.': [
-          'recxxxxxxx1'
+          'recxxxxxxx1',
         ],
         'What type(s) of support are you seeking?': [
           'Deliver groceries or supplies to me',
-          'Pick up a prescription for me'
+          'Pick up a prescription for me',
         ],
         'Cross Streets': '120th & 7th',
         'What borough/region are you in?': 'Bronx',
         'What neighborhood?': [
-          'recxxxxxxxxx2'
+          'recxxxxxxxxx2',
         ],
         'How soon do you need support?': 'In the next few days (3 - 5 Days)',
         'Anything else you want to share about your situation at this time?': 'Pie is great',
         'Timestamp Created': '2020-05-12T00:07:49.000Z',
         'Automation Logs': 'Dispatched: 11-05-2020 08:31 PM',
-        Name: 'John',
-        Meta: '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
+        'Name': 'John',
+        'Meta': '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
         'Airtable Gateway Last Processed': '2020-05-17T11:54:13.412Z',
         'Full Name': 'John Doe ',
         'Dispatched Time': '2020-05-12T00:25:32.000Z',
         'Automation Updated Timestamp': '2020-05-17T12:10:38.000Z',
         'AirTable Gateway Last Modified': '2020-05-17',
-        Status: 'In Progress - We Take Responsibility For This'
-      }
+        'Status': 'In Progress - We Take Responsibility For This',
+      },
     },
     {
       id: 'recxxxxxxxxxxx',
       fields: {
         'Unique ID': 985,
-        Cell: '(516) 555-1234',
+        'Cell': '(516) 555-1234',
         'Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.': [
-          'recxxxxxxx1'
+          'recxxxxxxx1',
         ],
         'What type(s) of support are you seeking?': [
           'Deliver groceries or supplies to me',
-          'Pick up a prescription for me'
+          'Pick up a prescription for me',
         ],
         'Cross Streets': '120th & 7th',
         'What borough/region are you in?': 'Bronx',
         'What neighborhood?': [
-          'recxxxxxxxxx2'
+          'recxxxxxxxxx2',
         ],
         'How soon do you need support?': 'In the next few days (3 - 5 Days)',
         'Anything else you want to share about your situation at this time?': 'Pie is great',
         'Timestamp Created': '2020-05-12T00:07:49.000Z',
         'Automation Logs': 'Dispatched: 11-05-2020 08:31 PM',
-        Name: 'John',
-        Meta: '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
+        'Name': 'John',
+        'Meta': '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
         'Airtable Gateway Last Processed': '2020-05-17T11:54:13.412Z',
         'Full Name': 'John Doe ',
         'Dispatched Time': '2020-05-12T00:25:32.000Z',
         'Automation Updated Timestamp': '2020-05-17T12:10:38.000Z',
         'AirTable Gateway Last Modified': '2020-05-17',
-        Status: 'In Progress - We Take Responsibility For This'
-      }
-    }
+        'Status': 'In Progress - We Take Responsibility For This',
+      },
+    },
   ],
   oneRecord: [
     {
       id: 'recxxxxxxxxxxx',
       fields: {
         'Unique ID': 985,
-        Cell: '(516) 555-1234',
+        'Cell': '(516) 555-1234',
         'Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.': [
-          'recxxxxxxx1'
+          'recxxxxxxx1',
         ],
         'What type(s) of support are you seeking?': [
           'Deliver groceries or supplies to me',
-          'Pick up a prescription for me'
+          'Pick up a prescription for me',
         ],
         'Cross Streets': '120th & 7th',
         'What borough/region are you in?': 'Bronx',
         'What neighborhood?': [
-          'recxxxxxxxxx2'
+          'recxxxxxxxxx2',
         ],
         'How soon do you need support?': 'In the next few days (3 - 5 Days)',
         'Anything else you want to share about your situation at this time?': 'Pie is great',
         'Timestamp Created': '2020-05-12T00:07:49.000Z',
         'Automation Logs': 'Dispatched: 11-05-2020 08:31 PM',
-        Note: 'some awesome note',
-        Name: 'John',
-        Meta: '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
+        'Note': 'some awesome note',
+        'Name': 'John',
+        'Meta': '{"lastValues":{"Unique ID":985,"Cell":"(516) 555-1234","Language Access: Do you prefer to communicate in a language other than English? If yes, please choose the language below.":["recxxxxxxxxx"],"What type(s) of support are you seeking?":["Deliver groceries or supplies to me","Pick up a prescription for me"],"Cross Streets":"120th & 7th","What borough/region are you in?":"Bronx","What neighborhood?":["recxxxxxxxx"],"How soon do you need support?":"In the next few days (3 - 5 Days)","Anything else you want to share about your situation at this time?":"Pie is great","Timestamp Created":"2020-05-12T00:07:49.000Z","Automation Logs":"Dispatched: 11-05-2020 08:31 PM","Name":"John","Full Name":"John Doe","Dispatched Time":"2020-05-12T00:25:32.000Z","Automation Updated Timestamp":"2020-05-17T11:53:51.000Z","AirTable Gateway Last Modified":"2020-05-17"}}',
         'Airtable Gateway Last Processed': '2020-05-17T11:54:13.412Z',
         'Full Name': 'John Doe ',
         'Dispatched Time': '2020-05-12T00:25:32.000Z',
         'Automation Updated Timestamp': '2020-05-17T12:10:38.000Z',
         'AirTable Gateway Last Modified': '2020-05-17',
-        Status: 'In Progress - We Take Responsibility For This'
-      }
+        'Status': 'In Progress - We Take Responsibility For This',
+      },
     },
-  ]
-}
+  ],
+};
+/* eslint-enable max-len */
