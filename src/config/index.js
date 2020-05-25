cc689a2ea40beae70bdc9cc2ed5d3ae4748c8e4e
Aaron Young <brainomite@gmail.com>
2020-05-25 08:18:37 -0400

    fix: Remove freezing of config variables
    This will resolve #26

--- src/config/index.js
+++ src/config/index.js
@@ -4,19 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config(); // load the local .env file
 }
 
-const deepFreeze = (object) => {
-  // Retrieve the property names defined on object
-  const propNames = Object.getOwnPropertyNames(object);
-  // Freeze properties before freezing self
-  for (const name of propNames) {
-    const value = object[name];
-    if (value && typeof value === 'object') {
-      deepFreeze(value);
-    }
-  }
-  return Object.freeze(object);
-};
-
 const config = {
   airtable: {
     key: process.env.AIRTABLE_KEY,
@@ -31,5 +18,4 @@ const config = {
   port: process.env.PORT,
 };
 
-// config values should never ever be mutated, lets make sure!
-module.exports = deepFreeze(config);
+module.exports = config;
