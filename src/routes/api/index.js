e881274852db638fd497bab41babd70f4182e655
Ben Moore <btmoore40@gmail.com>
2020-06-07 19:24:19 -0400

    Removing Colummn Mapping API as it is now done in configuration

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,17 +1,11 @@
 'use strict';
 const {Router} = require('express');
-const getColumnMapping = require('./getColumnMapping');
-const setColumnMapping = require('./setColumnMapping');
 const createRequest = require('./createRequest');
 const deleteRequest = require('./deleteRequest');
 
 module.exports = () => {
   const app = Router(); // eslint-disable-line new-cap
-
-  getColumnMapping(app);
-  setColumnMapping(app);
   createRequest(app);
   deleteRequest(app);
-
   return app;
 };
