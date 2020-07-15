7f1c23651727bbd473528c84c9a5d58a1a4b324f
Ben Moore <btmoore40@gmail.com>
2020-07-14 23:24:20 -0400

    Cleanup linting for emailSender File

--- src/service/emailSender.js
+++ src/service/emailSender.js
@@ -1,9 +1,16 @@
 const nodemailer = require('nodemailer');
 
-async function sendErrorEmail(account, password, sender, recipients, errorSubject, errorBody) {
+async function sendErrorEmail(
+    account,
+    password,
+    sender,
+    recipients,
+    errorSubject,
+    errorBody,
+) {
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -13,23 +20,37 @@ async function sendErrorEmail(account, password, sender, recipients, errorSubjec
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: sender, 
+  const info = await transporter.sendMail({
+    from: sender,
     to: recipients,
     subject: errorSubject,
     text: errorBody,
     html: errorBody,
   });
 
-  console.log("Message sent: %s", info.messageId);
+  console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-module.exports = (account, password, sender, recipients, errorSubject, errorBody) => {
-  console.log("Sending Error Email to: %s", recipients)
-  sendErrorEmail(account, password, sender, recipients, errorSubject, errorBody)
+module.exports = (
+    account,
+    password,
+    sender,
+    recipients,
+    errorSubject,
+    errorBody,
+) => {
+  console.log('Sending Error Email to: %s', recipients);
+  sendErrorEmail(
+      account,
+      password,
+      sender,
+      recipients,
+      errorSubject,
+      errorBody,
+  );
 };
